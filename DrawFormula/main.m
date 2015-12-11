9761a0de9870a9c7247b88310ddd907911fad7b2
hanlong <hanlong_wx@163.com>
2015-12-11 22:08:05 +0800

    Initial Commit

---
+++ DrawFormula/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DrawFormula
+//
+//  Created by hanlong on 15/12/11.
+//  Copyright © 2015年 hanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
